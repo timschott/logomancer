5311f01b94aa3cf27980707c07b7e99715d85ec4
timschott <tcs9pk@virginia.edu>
2019-12-28 13:58:58 -0500

    add server

---
+++ server/index.js
@@ -0,0 +1,40 @@
+// import express (after npm install express)
+const express = require('express');
+
+// create new express app and save it as "app"
+const app = express();
+
+// use hbs for front end
+const hbs = require('hbs')
+app.set('view engine', 'hbs')
+
+// set location of views directory - public/views.
+app.set('views', 'public/views/layout');
+
+// server configuration
+const PORT = 8080;
+
+// create a route for the app
+/*
+app.get('^((?!(\/|about|error)).)*$', function(req, res) {
+ res.redirect('/error');
+});
+*/
+
+app.get('/', function (req, res) {
+  res.render('home', {body: 'This is the body of the home page'})
+})
+
+app.get('/about', (req, res) => {
+	// inject into FE
+  res.render('about', { name: 'Timbo' })
+})
+
+// make another route. 
+app.get('/error', function (req, res) {
+  res.render('error', { problem: 'Error Page'})
+})
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
