8bc666601921905b0d3bb63d78f0c690b5c52645
timschott <tcs9pk@virginia.edu>
2019-12-29 23:16:07 -0500

    initial database setup

--- server/index.js
+++ server/index.js
@@ -4,30 +4,35 @@ const express = require('express');
 // create new express app and save it as "app"
 const app = express();
 
-// use hbs for front end
+//this required before view engine setup
+
+// assert that we're using hbs. 
+app.set('view engine', 'hbs');
+
 const hbs = require('hbs')
 
-//this required before view engine setup
 hbs.registerPartials('public/views/partials');
 
 // view engine setup
 app.set('views', 'public/views/layout');
 
-app.set('view engine', 'hbs');
-
-// set location of views directory - public/views.
-
-app.use(express.static('public'))
-
 // sets base url for use in .hbs files. 
 
-app.set("view options" , {baseURLEXT: "/public/views/layouts"});
+app.set("view options", {
+    baseURLEXT: "/public/views/layouts"
+});
+
+// set location of views directory - public/views.
+app.use(express.static('public'))
+
 // server configuration
 const PORT = 8080;
 
 // bodyParser for POSTs
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // create a route for the app
@@ -37,31 +42,55 @@ app.get('^((?!(\/|about|error)).)*$', function(req, res) {
 });
 */
 
+// database setup. 
+
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+
+// creds. grab from enviroment var file.
+require('dotenv').config({path:'.env'})
+
+var url = process.env.MONGO_URI;
+
+MongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('fun-words') // whatever your database name is
+    app.listen(PORT, () => {
+        console.log(`Server running at: http://localhost:${PORT}/`);
+    })
+})
+
 app.get('/', (req, res) => {
-  res.render('home', {body: 'This is the body of the home page'})
+    res.render('home', {
+        body: 'This is the body of the home page'
+    })
 })
 
 app.get('/about', (req, res) => {
-  // example of how to inject content into FE
-  res.render('about', { name: 'Timbo' })
+    // example of how to inject content into FE
+    res.render('about', {
+        name: 'Timbo'
+    })
 })
 
 // make another route. 
 app.get('/error', (req, res) => {
-  res.render('error', { problem: 'Error Page'})
+    res.render('error', {
+        problem: 'Error Page'
+    })
 })
 
 // add quotes. 
 app.get('/submit', (req, res) => {
-  res.render('submit')
+    res.render('submit')
 })
 
 // handler for that request.
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+    db.collection('words').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+    })
 })
-
-// make the server listen to requests
-app.listen(PORT, () => {
-  console.log(`Server running at: http://localhost:${PORT}/`);
-});
